7f889ab50251778a43554769010971eb1a5b7949
LeandroGelain03 <leandrogelain08@outlook.com>
2019-05-12 20:55:30 -0300

    Update secundary scrapy

--- ProjetoB3/EmpresasB3/FindCompany.py
+++ ProjetoB3/EmpresasB3/FindCompany.py
@@ -5,20 +5,22 @@ import requests
 import time
 import random
 import json
+import re
 import mongoConnect
+
+def mongoConection():
+    conexão, mydb = mongoConnect.connect()
+    cursor = mydb['empresas_link_b3']
+    return cursor
 
 def findAllCompanies():
     # driver.get('http://www.b3.com.br/pt_br/produtos-e-servicos/negociacao/renda-variavel/empresas-listadas.htm')
-    conexão, mydb = mongoConnect.connect()
-    cursor = mydb['WorkClusterFree']
+    cursor = mongoConection()
     arrayLinks = []
     arrayEmpresas = []
     subXpath= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Z','5']
-    for i in range(0,5):
-    # for i in range(0,len(subXpath)):
-        url = requests.get('http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/BuscaEmpresaListada.aspx?Letra='+str(subXpath[i])+'&idioma=pt-br')
-        # time.sleep(4)
-        
+    for i in range(0,len(subXpath)):
+        url = requests.get('http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/BuscaEmpresaListada.aspx?Letra='+str(subXpath[i])+'&idioma=pt-br')        
         pageContent = BeautifulSoup(url.content, 'lxml')
         for tagA in pageContent.find_all('a', href=True):
             nomeEmpresa = (tagA.text)
@@ -28,14 +30,40 @@ def findAllCompanies():
                 arrayEmpresas.append(nomeEmpresa)
                 print(tagA['href'] + ' ; '+tagA.text)
         
-    # for insert in range(0,len(arrayLinks)):
-    #     print('Inseridos banco: '+str(insert+1))
-    #     mydb.empresas_link_b3.insert_one({
-    #         "nome_empresa": arrayEmpresas[insert] , "link_b3_empresa": arrayLinks[insert]
-    #     })
-
-    # driver.close()
-    
-findAllCompanies()
+    for insert in range(0,len(arrayLinks)):
+        print('Inseridos banco: '+str(insert+1))
+        mydb.empresas_link_b3.insert_one({
+            "nome_empresa": arrayEmpresas[insert] , "link_b3_empresa": arrayLinks[insert]
+        })
 
 # linkBase na segunda pesquisa = http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/
+def find_details_in_all_companies():
+    url = ('http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/')
+    cursor = mongoConection()
+    ConcLinks = []
+    for links in cursor.find():
+        link = (links['link_b3_empresa'])
+        ConcLinks.append(link)
+        # print(link)
+    for i in range(0, 1):
+        linkSplit = (ConcLinks[1])
+        link = linkSplit.split('=')
+        codigo = (link[1])
+        listTD = []
+        url = requests.get('http://bvmf.bmfbovespa.com.br/pt-br/mercados/acoes/empresas/ExecutaAcaoConsultaInfoEmp.asp?CodCVM='+str(codigo)+'&ViewDoc=1&AnoDoc=2019&VersaoDoc=1&NumSeqDoc=80335')
+        pageContent = BeautifulSoup(url.content, 'lxml')                                  
+        # print(pageContent)
+        for tbody in pageContent.findAll('div',{'class':'content active'}):
+            # print(tbody)
+            for tr in tbody.findAll('tr'):
+                tr = (tr.text)
+                print(tr)
+                listTD.append(tr)
+                print('-=============================-')
+        # p = (listTD[0].text)
+        # p = p.split(':')
+        # p = str(p)
+        # p = p.replace('\n','')
+        # p = re.sub('A','',p)
+        # print(p)
+find_details_in_all_companies()
--- ProjetoB3/EmpresasB3/mongoConnect.py
+++ ProjetoB3/EmpresasB3/mongoConnect.py
@@ -1,7 +1,7 @@
 import pymongo
 
 def connect():
-    conexaoBD = pymongo.MongoClient('mongodb://leandrogelain08:M.a.159730@cluster0-shard-00-00-o7elb.mongodb.net:27017,cluster0-shard-00-01-o7elb.mongodb.net:27017,cluster0-shard-00-02-o7elb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
-    myDB = conexaoBD['pessoas-linkedin-data']
+    conexaoBD = pymongo.MongoClient('mongodb://leandrogelain2300:M.a.159730@myworkspace-shard-00-00-udqoj.mongodb.net:27017,myworkspace-shard-00-01-udqoj.mongodb.net:27017,myworkspace-shard-00-02-udqoj.mongodb.net:27017/test?ssl=true&replicaSet=MyWorkspace-shard-0&authSource=admin&retryWrites=true')
+    myDB = conexaoBD['Projetos-pessoais']
 
     return conexaoBD, myDB
