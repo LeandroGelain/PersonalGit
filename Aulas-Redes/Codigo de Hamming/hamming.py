ab13c9d428b4deecf4e1491098ee8484a0666a21
LeandroGelain03 <leandrogelain08@outlook.com>
2019-08-22 23:35:05 -0300

    Hamming ATT

--- Aulas-Redes/Codigo de Hamming/hamming.py
+++ Aulas-Redes/Codigo de Hamming/hamming.py
@@ -1,7 +1,34 @@
-numero = int(input('Numero pra binario:'))
-print("{0:b}".format(numero))
+import random
+class Hamming(object):
+    def __init__(self):
+        pass
 
-p1,
-p2,
-p4,
-p8
+    @classmethod
+    def bits(self, numero):
+        self.Bytes = ("{0:b}".format(numero))
+        self.array_bits = []
+        for self.bit in self.Bytes:
+            self.array_bits.append(int(self.bit))
+        return list(self.array_bits)
+    
+    @classmethod
+    def error_bit(self, array_bits):
+        print('===============================')
+        print('Array certo: ',array_bits)
+        self.random_position = random.randint(0,len(array_bits)-1)
+        print('Posição alterada',self.random_position)
+        if array_bits[self.random_position] == 0:
+            array_bits[self.random_position] = 1
+        elif array_bits[self.random_position] == 1:
+            array_bits[self.random_position] = 0
+            
+        print('Array com erro',array_bits)
+        print('===============================')
+        return list(array_bits)  
+    
+    
+if __name__ == '__main__':
+    numero = int(input('Numero pra binario:'))
+    list_bits = Hamming.bits(numero)
+    list_err = Hamming.error_bit(list_bits)
+    
