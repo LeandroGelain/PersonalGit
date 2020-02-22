be8154c560ba199b61c9d179e64f441ce9d3834e
LeandroGelain03 <leandrogelain08@outlook.com>
2020-02-22 14:36:09 -0300

    Init projeto de Marketing

--- 2020/ProjetoMarketing/instaBot/main.py
+++ 2020/ProjetoMarketing/instaBot/main.py
@@ -1,7 +1,7 @@
 from functions import InstagramBot
 import json
 if __name__ == "__main__":
-	bot = InstagramBot("evolui_mtk", "M.a.159730")
+	bot = InstagramBot("user", "pwd")
 	with open("hashtags.json") as dataJson:
 		data = json.load(dataJson)
 		for getHashTags in data["data"][0]["empreendedorismo"]:
